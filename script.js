74e776d5933330bdfffba488e205d4f2fd2db758
Zihui Yang <yangahq@bc.edu>
2021-10-16 19:00:31 -0400

    placeholder

--- script.js
+++ script.js
@@ -6,9 +6,23 @@ async function loadData(url){
 }
 
 async function main(){
+    //load data
     const url = "airports.json";
     data = await loadData(url);
     console.log(data);
+
+    //placeholder
+    let margin = {top: 20, right: 10, bottom: 20, left: 45};
+    const width = 650 - margin.left - margin.right;
+    const height = 600 - margin.top - margin.bottom;
+    const svg = d3.select("force-chart")
+                    .append('svg')
+                    .attr('width', width + margin.left + margin.right)
+                    .attr('height', height + margin.top + margin.bottom)
+                    .append("g")
+                    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    
 }
 
 main();
