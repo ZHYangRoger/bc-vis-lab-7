ad96cfb59be875f673362572ddc2c93e00584c5f
Zihui Yang <yangahq@bc.edu>
2021-10-18 12:33:40 -0400

    scale

--- script.js
+++ script.js
@@ -22,7 +22,16 @@ async function main(){
                     .append("g")
                     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    
+    //scale for size
+    let pop = []
+    data.nodes.forEach(a => pop.push(a.passengers));
+    let popRange = d3.extent(pop);
+    let popScale = d3.scaleLinear()
+                    .domain([popRange[0], popRange[1]])
+                    .range([10, 18])
+
+    //create force simulation
+    let simulation = d3.forceSimulation
 }
 
 main();
